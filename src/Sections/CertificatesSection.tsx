9fac11b73552775d2d1ac125cb9a5d3c902dd74e
Orel7777 <orelbukris7777@gmail.com>
2026-01-08 13:13:13 +0200

    8.1.2026

--- src/Sections/CertificatesSection.tsx
+++ src/Sections/CertificatesSection.tsx
@@ -306,7 +306,7 @@ const CertificatesSection: React.FC = () => {
                         aria-label="תעודה קודמת"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
-                          <path d="m9 18 6-6-6-6" />
+                          <path d="m15 18-6-6 6-6" />
                         </svg>
                       </button>
 
@@ -317,7 +317,7 @@ const CertificatesSection: React.FC = () => {
                         aria-label="תעודה הבאה"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
-                          <path d="m15 18-6-6 6-6" />
+                          <path d="m9 18 6-6-6-6" />
                         </svg>
                       </button>
                     </div>
@@ -384,7 +384,7 @@ const CertificatesSection: React.FC = () => {
                       aria-label="תעודה קודמת"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
-                        <path d="m9 18 6-6-6-6" />
+                        <path d="m15 18-6-6 6-6" />
                       </svg>
                     </button>
 
@@ -395,7 +395,7 @@ const CertificatesSection: React.FC = () => {
                       aria-label="תעודה הבאה"
                     >
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
-                        <path d="m15 18-6-6 6-6" />
+                        <path d="m9 18 6-6-6-6" />
                       </svg>
                     </button>
                   </div>
